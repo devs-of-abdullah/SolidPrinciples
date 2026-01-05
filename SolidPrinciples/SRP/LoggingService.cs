7b4aa7d801aad6d4b0f9c8d50fcaad7db22966be
Ahmet Abdullah <realahmetabdullah@gmail.com>
2026-01-06 01:48:02 +0300

    practiced SRP in LoggingService

---
+++ SolidPrinciples/SRP/LoggingService.cs
@@ -0,0 +1,65 @@
+﻿public class LoggingService
+{
+    public enum enLoggingType { ToFile, ToEventLog, ToDatabase }
+
+    public void Log(string message, enLoggingType LoggingType)
+    {
+
+        if (LoggingType == enLoggingType.ToFile)
+        {
+            FileLoggingService.Log(message);
+        }
+        else if (LoggingType == enLoggingType.ToEventLog)
+        {
+            EventLogService.Log(message);
+        }
+        else if (LoggingType == enLoggingType.ToDatabase)
+        {
+            DatabaseLoggingService.Log(message);
+        }
+    }
+
+
+
+public class FileLoggingService
+{
+    public static void Log(string message)
+    {
+        Console.WriteLine($"\nLog to file: {message}");
+    }
+}
+
+public class EventLogService
+{
+    public static void Log(string message)
+    {
+        Console.WriteLine($"\nLog to Event Log: {message}");
+    }
+}
+
+public class DatabaseLoggingService
+{
+
+    public static void Log(string message)
+    {
+        Console.WriteLine($"\nLog to Database: {message}");
+    }
+}
+
+
+    public void ExecuteClass()
+    {
+    
+        LoggingService LoggingService = new LoggingService();
+
+        LoggingService.Log("Error Occured line xxx.", LoggingService.enLoggingType.ToFile);
+
+        LoggingService.Log("Error Occured line xxx.", LoggingService.enLoggingType.ToEventLog);
+
+        LoggingService.Log("Error Occured line xxx.", LoggingService.enLoggingType.ToDatabase);
+
+        Console.ReadKey();
+
+    
+    }
+}
